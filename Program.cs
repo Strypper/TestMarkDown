1960db84475e990aaddecd01cc300e93d6c22a80
Strypper Vandel Jason <ocgrb.strypperjason115@gmail.com>
2023-07-25 21:29:42 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Markdig;
+using Markdig.Syntax;
+using Markdig.Syntax.Inlines;
+
+namespace MarkdownExample;
+    public static class Program
+    {
+        public static void Main()
+        {
+            var markdown = File.ReadAllText("TestMarkDown.md");
+
+            markdown = markdown.Replace("\r\n", Environment.NewLine);
+
+            var pipeline = new MarkdownPipelineBuilder().Build();
+            var document = Markdown.Parse(markdown, pipeline);
+
+            var headers = document.Descendants<HeadingBlock>();
+
+            foreach (var header in headers)
+            {
+                var inline = header?.Inline?.FirstOrDefault();
+                if (inline is LiteralInline literalInline)
+                {
+                    Console.WriteLine(literalInline.Content);
+                }
+            }
+        }
+    }
+
---
+++ TestMarkdown.md
@@ -0,0 +1,24 @@
+## Feature Request: [Feature A]
+
+### Description
+
+[Provide a detailed description of the feature you're requesting, including any background information or context that might be helpful.]
+
+### User Story
+
+[Describe how this feature will benefit users. What problem does it solve? How will it improve their experience?]
+
+### Acceptance Criteria
+
+- [List the specific requirements that must be met for this feature to be considered complete.]
+
+### Additional Information
+
+| Field       | Value |
+|-------------|-------|
+| Priority    | [High/Medium/Low] |
+| Effort      | [High/Medium/Low] |
+| Risk        | [High/Medium/Low] |
+| Business Value | [High/Medium/Low] |
+
+[Include any additional information that might be relevant, such as mockups, screenshots, or links to related issues or discussions.]
